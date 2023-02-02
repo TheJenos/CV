925f048d4f33f2e719999b443dbda6e2c13d5112
Thanura Nadun <nadunnew@gamil.com>
2023-02-02 19:29:51 +0530

    add ci/cd

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-import puppeteer from 'puppeteer'
-import path from 'path'
-import fs from 'fs'
+import puppeteer from "puppeteer";
+import server from "http-server";
+import path from "path";
 
 (async () => {
-
-  // Create a browser instance
-  const browser = await puppeteer.launch();
-
-  // Create a new page
-  const page = await browser.newPage();
-
-  // Open URL in current page
-  await page.goto("http://127.0.0.1:5173/", { waitUntil: 'networkidle0' }); 
-
-  //To reflect CSS used for screens instead of print
-  await page.emulateMediaType('screen');
-
-// Downlaod the PDF
-  const pdf = await page.pdf({
-    path: 'result.pdf',
-    margin: { top: '10px', right: '5px', bottom: '10px', left: '5px' },
-    printBackground: true,
-    format: 'A4',
+  const server = server.createServer({
+    root: path.join(process.cwd(), "dist"),
   });
 
-  // Close the browser instance
-  await browser.close();
+  server.listen(8080, "0.0.0.0", async function () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://127.0.0.1:8080/", { waitUntil: "networkidle0" });
+    await page.emulateMediaType("screen");
+    await page.pdf({
+      path: "cv.pdf",
+      margin: { top: "10px", right: "5px", bottom: "10px", left: "5px" },
+      printBackground: true,
+      format: "A4",
+    });
+    await browser.close();
+    server.close();
+  });
 })();
